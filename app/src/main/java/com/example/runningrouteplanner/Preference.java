d09178325955843b76c2b84f5b000c7a1d9f0803
Zhang Ting <zhangting.199612@gmail.com>
2019-10-20 16:49:48 -0400

    modify preference

--- app/src/main/java/com/example/runningrouteplanner/Preference.java
+++ app/src/main/java/com/example/runningrouteplanner/Preference.java
@@ -1,8 +1,16 @@
 package com.example.runningrouteplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class Preference extends AppCompatActivity {
 
@@ -10,5 +18,23 @@ public class Preference extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference);
+    }
+
+    public void onStartClick(View view){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+            Context context = getApplicationContext();
+            CharSequence text = "Location permission denied!\nPlease open it in setting!";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }else {
+            Intent intent = new Intent(Preference.this, MapsActivity.class);
+            startActivity(intent);
+            Context context = getApplicationContext();
+            CharSequence text = "Open the Map!";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
     }
 }
