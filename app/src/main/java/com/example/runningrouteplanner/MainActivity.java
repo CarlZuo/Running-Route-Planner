bbfd3ed90ca9f00f2810d8996189b2f61915d943
leigonhoo <895328154@qq.com>
2019-10-20 18:15:22 -0400

    add UI background

--- app/src/main/java/com/example/runningrouteplanner/MainActivity.java
+++ app/src/main/java/com/example/runningrouteplanner/MainActivity.java
@@ -16,13 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button_01 = (Button) findViewById(R.id.button01);//id后面为上方button的id
+        button_01 = (Button) findViewById(R.id.button01);
 
         button_01.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(com.example.runningrouteplanner.MainActivity.this, Preference.class);//this前面为当前activty名称，class前面为要跳转到得activity名称
+                intent.setClass(com.example.runningrouteplanner.MainActivity.this, Preference.class);
                 startActivity(intent);
             }
         });
